dee934bc66a3d56d35dce2ed832ca1d57aff7cd6
Jônatas da Silva <jonatasdasilvasantos@outlook.com>
2025-06-01 13:07:27 -0300

    ♻️ refactor: Remove domain in configuration

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
-		domains: ["avatar.iran.liara.run"],
 		remotePatterns: [new URL("https://avatar.iran.liara.run/**")],
 	},
 };
