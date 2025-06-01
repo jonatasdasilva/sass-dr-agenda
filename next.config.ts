5698f42a988d6e93990bb7828b32513810d6a09b
Jônatas da Silva <jonatasdasilvasantos@outlook.com>
2025-06-01 13:13:56 -0300

    Revert "♻️ refactor: Remove domain in configuration"
    This reverts commit dee934bc66a3d56d35dce2ed832ca1d57aff7cd6.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: {
+		domains: ["avatar.iran.liara.run"],
 		remotePatterns: [new URL("https://avatar.iran.liara.run/**")],
 	},
 };
