7811a49c4a5155024aa1ce499d3cb5f358444f52
Jônatas da Silva <jonatasdasilvasantos@outlook.com>
2025-06-01 12:47:45 -0300

    🔧 chore: Insert setup of server remote for image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	images: {
+		domains: ["avatar.iran.liara.run"],
+		remotePatterns: [new URL("https://avatar.iran.liara.run/**")],
+	},
 };
 
 export default nextConfig;
